13ab135fb4dbbcbadfada0a0c0fd13221da32847
César Aguilera <cesar.aguilera.p@gmail.com>
2025-05-14 21:30:32 +0200

    add bit test executable

--- bin/bits_test.rs
+++ bin/bits_test.rs
@@ -1,3 +1,21 @@
+use std::{env, io};
+use learning_rust::bits::str_to_bits;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let input = if args.len() == 2 {
+        args[1].trim()
+    } else {
+        println!("Enter something:");
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        &(input.trim().to_owned())
+    };
+
+    println!("{}", str_to_bits(input));
 }
