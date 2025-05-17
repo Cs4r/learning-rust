3c05273a83056841c60b7c46f0ecc573424ddd1c
César Aguilera <cesar.aguilera.p@gmail.com>
2025-05-17 11:20:51 +0200

    add test

--- src/codec.rs
+++ src/codec.rs
@@ -5,3 +5,28 @@ struct Codec {
     by_byte: HashMap<u8, BitVector>,
     by_vector : HashMap<BitVector, u8>,
 }
+
+impl Codec {
+    fn new() -> Self {
+        Codec {
+            by_byte: HashMap::new(),
+            by_vector: HashMap::new(),
+        }
+    }
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    mod constructor {
+        use super::*;
+        #[test]
+        fn test_new_creates_empty_codec() {
+            let codec = Codec::new();
+            assert_eq!(codec.by_byte.len(), 0);
+            assert_eq!(codec.by_vector.len(), 0);
+        }
+    }
+}
