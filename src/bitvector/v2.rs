31c0d395c10e48ca9e942d827f72249a0eb0f72c
César Aguilera <cesar.aguilera.p@gmail.com>
2025-05-18 12:17:34 +0200

    add tests for default constructor

--- src/bitvector/v2.rs
+++ src/bitvector/v2.rs
@@ -1,19 +1,16 @@
-
 struct BitVector {
     data: Vec<u8>,
-    n_bits: usize
+    n_bits: usize,
 }
 
-
 impl BitVector {
-    pub fn new(n_bits: usize) -> BitVector {
+    pub fn new() -> BitVector {
         BitVector {
             data: vec![0; 0],
-            n_bits
+            n_bits: 0,
         }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -23,8 +20,10 @@ mod tests {
         use super::*;
 
         #[test]
-        fn test_constructors() {
-
+        fn test_new_creates_empty_vector() {
+            let bv = BitVector::new();
+            assert_eq!(bv.data.len(), 0);
+            assert_eq!(bv.n_bits, 0);
         }
     }
 }
